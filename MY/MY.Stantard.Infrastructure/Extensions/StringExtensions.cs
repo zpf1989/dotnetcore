de793912d2cb9d01413730655b0ac63a0bf12d72
WORK_PC_ZPF <751682472@qq.com>
2017-09-19 13:31:21 +0800

    添加扩展文件；添加newtonsoft.json依赖

--- MY/MY.Stantard.Infrastructure/Extensions/StringExtensions.cs
+++ MY/MY.Stantard.Infrastructure/Extensions/StringExtensions.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Security.Cryptography;
+using System.Text;
+
 namespace MY.Stantard.Infrastructure.Extensions
 {
     public static class StringExtensions
@@ -11,6 +14,23 @@ namespace MY.Stantard.Infrastructure.Extensions
         public static string ToStringIfNull(this object obj, string defaultStr = "")
         {
             return obj == null ? (defaultStr == null ? "" : defaultStr) : obj.ToString();
+        }
+
+        //32ŒªMD5º”√‹
+        public static string MD5(this string str)
+        {
+            if (str.IsEmptyByString())
+            {
+                return "";
+            }
+            var md5Provider = new MD5CryptoServiceProvider();
+            byte[] data = md5Provider.ComputeHash(Encoding.Default.GetBytes(str));
+            var sb = new StringBuilder();
+            for (int idx = 0; idx < data.Length; idx++)
+            {
+                sb.Append(data[idx].ToString("x2"));
+            }
+            return sb.ToString();
         }
     }
 }
